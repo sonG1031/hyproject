7f39390424c105b205a5cbc699f88fe61263123f
송민섭 <minsub1031@gmail.com>
2022-05-20 23:56:53 +0900

    2022/05/20
    로그인, 로그아웃 기능 추가

--- hy/forms.py
+++ hy/forms.py
@@ -10,9 +10,13 @@ class AnswerForm(FlaskForm):
     content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
 
 class UserCreateForm(FlaskForm):
-    username = StringField('사용자 이름', validators=[DataRequired('사용자 이름은 항목입니다.'), Length(min=3, max=25)])
+    username = StringField('사용자 이름', validators=[DataRequired('사용자 이름은 필수입력 항목입니다.'), Length(min=3, max=25)])
     password1 = PasswordField('비밀번호', validators=[
         DataRequired('비밀번호는 필수입력 항목입니다.'), EqualTo('password2', '비밀번호가 일치하지 않습니다.')
     ])
     password2 = PasswordField('비밀번호 확인', validators=[DataRequired('비밀번호 확인은 필수입력 항목입니다.')])
     email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email("잘못된 이메일 주소 입니다.")])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자 이름', validators=[DataRequired("사용자 이름은 필수입력 항목입니다."), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired("비밀번호는 필수압력 항목입니다.")])
