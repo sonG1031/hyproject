72937727dfc606a2eb16b90a3023fd444cb48628
송민섭 <minsub1031@gmail.com>
2022-05-22 23:20:57 +0900

    update models
    db 모델 수정함.

--- hy/views/question_views.py
+++ hy/views/question_views.py
@@ -1,11 +1,12 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g
 from werkzeug.utils import redirect
 
 from hy import db
 from hy.models import Question
 from hy.forms import QuestionForm, AnswerForm
+from hy.views.auth_views import login_required
 
 bp = Blueprint('question', __name__, url_prefix='/question')
 
@@ -23,10 +24,11 @@ def detail(question_id):
     return render_template('question/question_detail.html', question=question, form = form)
 
 @bp.route("/create/", methods=('GET', 'POST'))
+@login_required
 def create():
     form = QuestionForm()
     if request.method == 'POST' and form.validate_on_submit():
-        question = Question(subject = form.subject.data, content = form.content.data, create_date = datetime.now())
+        question = Question(subject = form.subject.data, content = form.content.data, create_date = datetime.now(), user= g.user)
         db.session.add(question)
         db.session.commit()
         return redirect(url_for('main.index'))
